2d6594d7b5db6cb5373764eab8e6e78dfc9616aa
katsaGeorge <georgekatsabsn@gmail.com>
2023-11-02 10:50:36 +0200

    Atlas connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://<username>:<password>@cluster.lh8mqhc.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule,
   ],
   controllers: [AppController],
